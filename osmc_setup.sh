62d46ebe665608a56b36c334e440d7bc7051ade1
Chris Welsh <chriswelsh@pop-os.localdomain>
2019-07-08 23:17:03 +0100

    added sonarr repos

--- osmc_setup.sh
+++ osmc_setup.sh
@@ -47,5 +47,13 @@ install_sonarr () {
     wget https://mediaarea.net/repo/deb/repo-mediaarea_1.0-9_all.deb && \
     dpkg -i repo-mediaarea_1.0-9_all.deb && apt-get update
 
-    
+    # Add SOnarr Repo
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
+    --recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8
+    echo "deb https://apt.sonarr.tv/debian stretch main" | \
+    sudo tee /etc/apt/sources.list.d/sonarr.list
+    sudo apt update
+
+    # Install Sonarr
+    sudo apt install sonarr
 }
