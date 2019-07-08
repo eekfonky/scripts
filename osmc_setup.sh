334dcd320748216c97e6963f6919d2401538fbf0
Chris Welsh <chriswelsh@pop-os.localdomain>
2019-07-08 23:16:42 +0100

    added sonarr

--- osmc_setup.sh
+++ osmc_setup.sh
@@ -32,3 +32,20 @@ sudo systemctl enable nzbget
 sudo systemctl start nzbget
 sudo systemctl status nzbget
 }
+
+## Sonarr
+install_sonarr () {
+    # Install Mono Repo
+    sudo apt install apt-transport-https dirmngr gnupg ca-certificates
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
+    --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    echo "deb https://download.mono-project.com/repo/debian stable-stretch main" | \
+    sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+    sudo apt update
+
+    # Install mediaInfo Repo
+    wget https://mediaarea.net/repo/deb/repo-mediaarea_1.0-9_all.deb && \
+    dpkg -i repo-mediaarea_1.0-9_all.deb && apt-get update
+
+    
+}
