af4668c6742c5476f05841d980eb74df82e0aa8f
Chris Welsh <chriswelsh@pop-os.localdomain>
2019-07-08 22:44:29 +0100

    added nzbget function

--- osmc_setup.sh
+++ osmc_setup.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
+
+## NZBGet
+install_nzbget () {
 sudo mkdir -r /opt/nzbget && sudo chown -R osmc:osmc /opt/nzbget
 wget https://nzbget.net/download/nzbget-latest-bin-linux.run -P /tmp
 chmod +x /tmp/nzbget-latest-bin-linux.run
 sh /tmp/nzbget-latest-bin-linux.run --destdir /opt/nzbget
 
 
+#sudo systemctl list-units --type=mount
+#sudo vim /etc/systemd/system/nzbget.service
+
 [Unit]
-Description=NZBGet Daemon
-Documentation=http://nzbget.net/Documentation
-After=network.target
+Description=NZBGet
+After=network.target #mnt-extHD.mount
 
 [Service]
-User=<replace_with_the_user_you_want>
-Group=<replace_with_the_group_you_want>
+User=osmc
+Group=osmc
 Type=forking
-ExecStart=</path/to/nzbget/nzbget> -D
-ExecStop=</path/to/nzbget/nzbget> -Q
-ExecReload=</path/to/nzbget/nzbget> -O
+ExecStart=/opt/nzbget/nzbget -D
+ExecStop=/opt/nzbget/nzbget -Q
+ExecReload=/opt/nzbget/nzbget -O
 KillMode=process
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
+
+sudo systemctl enable nzbget
+sudo systemctl start nzbget
+sudo systemctl status nzbget
+}
