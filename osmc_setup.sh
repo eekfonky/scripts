5e5dcd5e7f25a9b7b8267b542415098b036e0c60
Chris Welsh <chriswelsh@pop-os.localdomain>
2019-07-11 20:45:27 +0100

    update

--- osmc_setup.sh
+++ osmc_setup.sh
@@ -2,18 +2,18 @@
 
 ## NZBGet
 install_nzbget () {
-sudo mkdir -r /opt/nzbget && sudo chown -R osmc:osmc /opt/nzbget
-wget https://nzbget.net/download/nzbget-latest-bin-linux.run -P /tmp
-chmod +x /tmp/nzbget-latest-bin-linux.run
-sh /tmp/nzbget-latest-bin-linux.run --destdir /opt/nzbget
-
-
-#sudo systemctl list-units --type=mount
-#sudo vim /etc/systemd/system/nzbget.service
+    # Create directory & change permissions
+    sudo mkdir -r /opt/nzbget && sudo chown -R osmc:osmc /opt/nzbget
+    # Download nzbget latest to /tmp
+    wget https://nzbget.net/download/nzbget-latest-bin-linux.run -P /tmp
+    # Make executable
+    chmod +x /tmp/nzbget-latest-bin-linux.run
+    # Launch into /opt/nzbget
+    sh /tmp/nzbget-latest-bin-linux.run --destdir /opt/nzbget
 
 [Unit]
 Description=NZBGet
-After=network.target #mnt-extHD.mount
+After=network.target
 
 [Service]
 User=osmc
@@ -37,23 +37,23 @@ sudo systemctl status nzbget
 install_sonarr () {
     # Install Mono Repo
     sudo apt install apt-transport-https dirmngr gnupg ca-certificates
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
-    --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-    echo "deb https://download.mono-project.com/repo/debian stable-stretch main" | \
-    sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-    sudo apt update
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
+--recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/debian stable-stretch main" | \
+sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
 
-    # Install mediaInfo Repo
-    wget https://mediaarea.net/repo/deb/repo-mediaarea_1.0-9_all.deb && \
-    dpkg -i repo-mediaarea_1.0-9_all.deb && apt-get update
+# Install mediaInfo Repo
+wget https://mediaarea.net/repo/deb/repo-mediaarea_1.0-9_all.deb && \
+sudo dpkg -i repo-mediaarea_1.0-9_all.deb && apt-get update
 
-    # Add SOnarr Repo
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
-    --recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8
-    echo "deb https://apt.sonarr.tv/debian stretch main" | \
-    sudo tee /etc/apt/sources.list.d/sonarr.list
-    sudo apt update
+# Add Sonarr Repo
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
+--recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8
+echo "deb https://apt.sonarr.tv/debian stretch main" | \
+sudo tee /etc/apt/sources.list.d/sonarr.list
+sudo apt update
 
-    # Install Sonarr
-    sudo apt install sonarr
+# Install Sonarr
+sudo apt install sonarr
 }
